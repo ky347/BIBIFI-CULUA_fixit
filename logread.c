afcc692b07cd401dc1f66de3bb9d6e557e03545f
yolobots0318 <yolobots0318@gmail.com>
2026-03-20 21:43:02 -0400

    Patch 14

--- logread.c
+++ logread.c
@@ -46,7 +46,7 @@ void execute_records(Record *Rarr, unsigned int num_records, struct slisthead *h
     struct Person *temp;
     struct Person *found_person = NULL;
     SLIST_FOREACH(temp, head, link) {
-      if (strcmp(Rarr[i].name, temp->name) == 0 && Rarr[i].guestType == temp->guestType) {
+      if (strcmp(Rarr[i].name, temp->name) == 0) {
         //we've found a matching name that already exists in the linked list
         found_person = temp;
         break;
@@ -103,46 +103,31 @@ void execute_records(Record *Rarr, unsigned int num_records, struct slisthead *h
 void print_time(Record *Rarr, unsigned int num_records, char *name) {
   //TODO code this
   //printf("Called print_time\n");
-  //unsigned long min_time = 0;
-  //unsigned long max_time = 0;
-  unsigned long total_time = 0;
-  unsigned long last_arrival = 0;
-  int inside = 0;
+  unsigned long min_time = 0;
+  unsigned long max_time = 0;
   int found = 0;
 
   //iterate through Record array
   for (unsigned int i = 0; i < num_records; i++) {
     //check if the record belongs to the person we are looking for
-    if (strcmp(Rarr[i].name, name) == 0 && Rarr[i].room == -1) {
-      //unsigned long current_time = (unsigned long)Rarr[i].timestamp;
-      found = 1;
-      //if person arrived at gallery
-      if (Rarr[i].actionType == Arrived) {
-        last_arrival = (unsigned long)Rarr[i].timestamp;
-        inside = 1;
-      }
-      //if person left and was previously inside the gallery
-      else if (Rarr[i].actionType == Left && inside) {
-        total_time += ((unsigned long)Rarr[i].timestamp - last_arrival);
-        inside = 0;
-      }
+    if (strcmp(Rarr[i].name, name) == 0) {
+      unsigned long current_time = (unsigned long)Rarr[i].timestamp;
 
-      // if (!found) {
-      //   //if we haven't found this person yet, initialize with the first timestamp
-      //   min_time = current_time;
-      //   max_time = current_time;
-      //   found = 1;
-      // } else {
-      //   //update the min and max
-      //   if (current_time < min_time) min_time = current_time;
-      //   if (current_time > max_time) max_time = current_time;
-      // }
+      if (!found) {
+        //if we haven't found this person yet, initialize with the first timestamp
+        min_time = current_time;
+        max_time = current_time;
+        found = 1;
+      } else {
+        //update the min and max
+        if (current_time < min_time) min_time = current_time;
+        if (current_time > max_time) max_time = current_time;
+      }
     }
   }
 
   if (found) {
-    //printf("%lu", max_time - min_time);
-    printf("%lu", total_time);
+    printf("%lu", max_time - min_time);
   }
 
   return;
@@ -363,24 +348,19 @@ int main(int argc, char *argv[]) {
   //read data from logfile
   Buffer data = {NULL, 0};
   data = verify_then_decrypt(logpath, token, strlen(token));
-  if(data.Length == 0){
-    printf("integrity violation");
-    return 225;
-  }
-
   // printf("data.Length: %d\n", data.Length);
   // printf("data.Buf: %s\n", data.Buf);
   json_t *json = NULL;
   json = json_from_buf(data.Buf, data.Length);
   if(json == NULL){
-    printf("invalid\n");
+    printf("JSON is NULL\n");
     return 255;
   }
 
   Record *records = NULL;
   size_t num_records;
   if(!get_all_records(json, &records, &num_records)){
-    printf("invalid\n");
+    printf("Could not Parse JSON\n");
     return 255;
    }
   // for (int i = 0; i < num_records; i++){
